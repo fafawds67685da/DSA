402250cfd20e37072a5a6a8d79de14857ace043b
Dev goyal <158808178+fafawds67685da@users.noreply.github.com>
2024-11-10 11:46:35 +0530

    Create LL 39 ( To delete second largest node ).c

---
+++ 2. DS Singly Linked List/LL 39 ( To delete second largest node ).c
@@ -0,0 +1,55 @@
+sn*large2_del(sn*head)
+{
+    if(head==NULL)
+    {
+        printf("\t List is empty \n");
+        return head;
+    }
+    else if(head->next==NULL)
+    {
+        sn*p1=head->next;
+        head=NULL;
+        free(p1);
+        return head;
+    }
+    else
+    {
+        sn*p1=head,*p2=NULL,*p3=NULL,*p4=head,*p5=NULL,*p6=head;
+        int z=0,z2=0;
+        while(p1!=NULL)
+        {
+            if(p1->info>z)
+            {
+                z2=z;
+                z=p1->info;
+                p5=p3;
+                p3=p2;
+                p6=p4;
+                p4=p1;
+            }
+            else
+            {
+                if(p1->info>z2)
+                {
+                    z2=p2->info;
+                    p5=p2;
+                    p6=p1;
+                }
+            }
+            p2=p1;
+            p1=p1->next;
+        }
+        if(p5==NULL)
+        {
+            head=p6->next;
+            free(p6);
+            return head;
+        }
+        else
+        {
+            p5->next=p6->next;
+            free(p6);
+            return head;
+        }
+    }
+}

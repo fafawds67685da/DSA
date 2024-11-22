7fb55c3f8fbad26ad2b9d335f9761d8192a868e5
Dev goyal <158808178+fafawds67685da@users.noreply.github.com>
2024-11-22 18:33:35 +0530

    Update T 07 ( find the second smallest info in the tree ).c

--- 6. Tree/T 07 ( find the second smallest info in the tree ).c
+++ 6. Tree/T 07 ( find the second smallest info in the tree ).c
@@ -2,22 +2,20 @@ void small_leaf(tree *root,int *y,int *c)
 {
     if(root!=NULL)
     {
-        if(*y > root->info)
-        {
 
-            *c=*y;
-            *y=root->info;
-
-        }
         small_leaf(root->left,y,c);
-
+        *y  =*y + 1;
+        if(*y==2)
+        {
+            *c=root->info;
+        }
         small_leaf(root->right,y,c);
     }
 }
 
         case 7:
-            y=100000;
-            int z=100000;
+            y=0;
+            int z=0;
             small_leaf(root,&y,&z);
             printf("\t second smallest node in the tree is : %d \n",z);
             break;

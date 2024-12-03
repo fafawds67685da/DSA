9f028f858409dfd58fd3b1ee47fec55c21ccb2a6
Dev goyal <158808178+fafawds67685da@users.noreply.github.com>
2024-12-03 15:48:46 +0530

    Update S6 ( Merge Sort ).c

--- 09. Sorting (Psuedo codes  )/S6 ( Merge Sort ).c
+++ 09. Sorting (Psuedo codes  )/S6 ( Merge Sort ).c
@@ -1,55 +1,41 @@
-void merge(char *p, int l)
+void merge(int* a, int l, int h)
 {
-    if (l > 1)
+    if(l<h)
     {
-        int mid = l / 2;
-
-        merge(p, mid);
-        merge(p + mid, l - mid);
-
-        mergesort(p, 0, mid, l);
+        int mid = (l+h)/2;
+        merge(a,l,mid);
+        merge(a,mid+1,h);
+        sort(a,l,mid,h);
     }
 }
+ void sort(int* a, int l, int mid, int h)
+ {
+     int b[100];
+     int i=l,j=mid+1,k=l;
+     while(i<=mid && j<=h)
+     {
+         if(a[i]<a[j])
+         {
+             b[k++]=a[i++];
+         }
+         else
+         {
+             b[k++]=a[j++];
+         }
+     }
+     while(i<=mid)
+     {
+         b[k++]=a[i];
+         i++;
+     }
 
-void mergesort(char *p, int low, int mid, int high)
-{
-    int left_size = mid;
-    int right_size = high - mid;
-
-    char left[left_size], right[right_size];
-
-    for (int i = 0; i < left_size; i++)
-        left[i] = p[low + i];
-    for (int j = 0; j < right_size; j++)
-        right[j] = p[mid + j];
-
-    int i = 0, j = 0, k = low;
-    while (i < left_size && j < right_size)
-    {
-        if (left[i] <= right[j])
-        {
-            p[k] = left[i];
-            i++;
-        }
-        else
-        {
-            p[k] = right[j];
-            j++;
-        }
-        k++;
-    }
-
-    while (i < left_size)
-    {
-        p[k] = left[i];
-        i++;
-        k++;
-    }
-
-    while (j < right_size)
-    {
-        p[k] = right[j];
-        j++;
-        k++;
-    }
-}
+     while(j<=h)
+     {
+         b[k++]=a[j];
+         j++;
+     }
+     for(i=l;i<=h;i++)
+     {
+         a[i]=b[i];
+     }
+ }
